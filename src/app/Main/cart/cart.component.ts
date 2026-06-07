a728744bd545fbb647d7c94708eba4704c57bd1f
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-07 13:51:50 +0530

    cart component

--- src/app/Main/cart/cart.component.ts
+++ src/app/Main/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+
+  cartItems: any[] = [];
+
+  constructor(private cartService: CartService) {
+
+  }
+
+  ngOnInit() {
+
+    this.loadCart();
+
+  }
+
+  loadCart() {
+    this.cartItems = this.cartService.getCartItems();
+  }
+
+  increaseQty(product: any) {
+    this.cartService.increaseQty(product._id);
+  }
+
+  decreaseQty(product: any) {
+    this.cartService.decreaseQty(product._id);
+    this.loadCart();
+  }
+
+  getSubtotal() {
+    return this.cartItems.reduce(
+      (total, item) =>
+        total +
+        (item.offerPrice *
+          item.quantity),
+      0
+    );
+  }
+
+  // getDiscount(){
+  //   return Math.round(
+  //     this.getSubtotal() * 0.10
+  //   );
+  // }
+
+  getGrandTotal() {
+    if (this.getSubtotal() < 500) {
+      return this.getSubtotal() + 99; // Apply shipping charge
+    }
+    return this.getSubtotal();
+  }
+
+  getTotalItems(): number {
+    return this.cartService.getCartCount();
+  }
 
 }
