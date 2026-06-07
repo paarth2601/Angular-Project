722100808458a49dbb2051589a8d2f6d10292623
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-07 13:51:02 +0530

    header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  cartCount: number = 0;
+  menuOpen = false;
+
+  constructor(private cartService: CartService) {
+
+  }
+
+
+  ngOnInit() {
+    this.loadCartCount();
+
+    this.cartService.cartUpdated.subscribe(count => {
+        this.cartCount = count;
+      });
+  }
+
+  loadCartCount() {
+
+    this.cartCount =
+      this.cartService.getCartCount();
+
+  }
+
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
 
 }
