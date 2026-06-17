7d2d4f3985265e0254a522ae2e386bda9acfd8eb
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-17 12:52:47 +0530

    contact component

--- src/app/Main/contact/contact.component.ts
+++ src/app/Main/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  contactData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+  messageSent: boolean = false;
+
+  constructor(private contactService: ContactService) {
+
+  }
+
+
+  openWhatsapp() {
+
+    const message =
+      encodeURIComponent(
+        'Hello, I would like to know more about your products.'
+      );
+
+    window.open(
+      `https://wa.me/917889505059?text=${message}`,
+      '_blank'
+    );
+
+  }
+
+
+  submitForm() {
+    this.contactService.sendMessage(this.contactData).subscribe({
+        next: () => {
+          // alert(
+          //   'Message sent successfully'
+          // );
+          this.messageSent = true;
+          this.contactData = { name: '', email: '', message: '' };
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+
+  }
 
 }
