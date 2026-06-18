ec9a90f45d510264a429f2420b9a77891f8fcaa8
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-18 15:42:02 +0530

    product component

--- src/app/Main/product/product.component.ts
+++ src/app/Main/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +9,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  products: any[] = [];
+  filteredProducts: any[] = [];
+  selectedCategory = 'All Products';
+  cartItems: { [key: string]: number } = {};
+
+  categories = [
+    'All Products',
+    'Walnuts',
+    'Almonds',
+    'Mamra Badam',
+    'Jammu Rajma',
+    'Kashmiri Kesar'
+  ];
+
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.loadProducts();
+    this.loadCartItems();
+  }
+
+
+  loadProducts(): void {
+    this.productService.getProducts().subscribe({
+      next: (res: any) => {
+        this.products = res;
+        this.filteredProducts = res;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  filterProducts(category: string): void {
+    this.selectedCategory = category;
+    if (category === 'All Products') {
+      this.filteredProducts = this.products;
+      return;
+    }
+    this.filteredProducts = this.products.filter(
+      product => product.category === category
+    );
+  }
+
+
+  // CART CODE
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    this.loadCartItems();
+  }
+
+  increaseQty(product: any) {
+    this.cartService.increaseQty(product._id);
+    this.loadCartItems();
+  }
+
+  decreaseQty(product: any) {
+    this.cartService.decreaseQty(product._id);
+    this.loadCartItems();
+  }
+
+
+  loadCartItems() {
+    const cart =
+      this.cartService.getCartItems();
+    this.cartItems = {};
+    cart.forEach(item => {
+      this.cartItems[item._id] =
+        item.quantity;
+    });
+  }
+
 
 }
