7227c1d10fe07822db054637fcc88cff48386a4d
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-30 00:40:03 +0530

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environment';
+import { environment } from 'src/environment.prod';
 
 @Injectable({
   providedIn: 'root'
