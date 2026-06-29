3ffeed9f88bc35c6451d7182b6b62f3434c69fdb
Paarth Gupta <paarthgupta7@gmail.com>
2026-06-29 16:10:22 +0530

    env files

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
+  private apiUrl =
+      environment.apiUrl + '/contact';
+      
   constructor(private http: HttpClient) { }
 
   sendMessage(data: any) {
 
-    return this.http.post(
-      'http://localhost:5000/api/contact',
+    return this.http.post(this.apiUrl,
       data
     );
 
